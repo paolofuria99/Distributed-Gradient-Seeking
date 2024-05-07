2473f91b14f7e4dff6f43142493a3d525e83f51b
paolofuria99 <paofuria@gmail.com>
2024-05-07 21:46:54 +0200

    modified init

--- init.m
+++ init.m
@@ -1,4 +1,4 @@
-%% Initialization script
+%% Initialization
 close all;
 clear all;
 clc;
@@ -17,28 +17,3 @@ set(0,'defaultAxesFontSize', 20);
 set(0,'DefaultLegendFontSize', 20);
 set(0,'DefaultFigureWindowStyle','normal');
 set(0,'DefaultUicontrolFontsize', 14);
-
-
-% Simulation parameters
-sim_param = struct(...
-    'dt',            0.1,        ...    % [s]   Time step
-    'max_iter',      1e4,        ...    % [-]   Maximum number of iterations
-    'N_agents',      3,          ...    % [-]   Number of agents
-    'ENV_SIZE',      60,         ...    % [m]   Size of the environment
-    'AGENT_RADIUS',  1,        ...    % [m]   Agent radius
-    'WHEEL_RADIUS',  0.01,       ...    % [m]   Wheel radius
-    'AXLE_LENGTH',   0.05,       ...    % [m]   Inter-Axle length
-    'std_dyn_xy',    0.1,        ...    % [m]   Standard deviation of the dynamic noise
-    'std_dyn_theta', 1*(pi/180), ...    % [rad] Standard deviation of the dynamic noise
-    'std_gps',       0.1,        ...    % [m]   Standard deviation of the GPS noise
-    'MAX_LIN_VEL',   1e-6,       ...    % [m/s] Maximum linear velocity
-    'MAX_LIN_ACC',   0.5,        ...    % [m/s^2] Maximum linear acceleration
-    'MAX_ANG_VEL',   0.3,        ...    % [rad/s] Maximum angular velocity
-    'GAINS',         [1 1],      ...    % [-]   Gains of the forward control
-    'RADIUS_SEPARATION', 0.2,    ...    % [m] Minimum separation distance between agents
-    'RADIUS_ALIGNMENT', 0.3,     ...    % [m] Radius of the alignment function
-    'RADIUS_COHESION', 0.4,      ...    % [m] Radius of the cohesion function
-    'FORCE_SEPARATION', 0.1,     ...    % [N] Force of the separation function
-    'FORCE_ALIGNMENT', 0.2,      ...    % [N] Force of the alignment function
-    'FORCE_COHESION', 0.3        ...    % [N] Force of the cohesion function
-    );
