0b967f0f746811c56410364e72e036579700c3fd
paolofuria99 <paofuria@gmail.com>
2024-06-10 18:51:39 +0200

    Added further plots

--- functions/plot_simulation.m
+++ functions/plot_simulation.m
@@ -1,5 +1,5 @@
 %%  Plot Simulation
-% This script plots the simulation results of the unicycle robot 
+% This script plots the simulation results of the distributed gradient seeking unicycle robot 
 
 %% Animate the simulation
 if ANIMATE
@@ -88,7 +88,7 @@ figure
     linkaxes([ax1,ax2,ax3,ax4,ax5,ax6],'x')
 
 
-% Additional visualization: plot trajectory on the scalar field
+% Plot trajectory on the scalar field
 figure
     hold on
     contour(X, Y, Z, 30, 'DisplayName','Scalar Field')
@@ -101,5 +101,99 @@ figure
     title('Unicycle Robot Trajectory with ESC')
     legend('Location','best')
     grid on
+
+% Plot the error ellipses
+figure
+    hold on
+    contour(X, Y, Z, 30, 'DisplayName','Scalar Field')
+    colorbar
+    for i=1:iter_break
+        % Plot the error ellipses, plot the first and every 5th ellipse
+        if mod(i,5)==0
+            plot_ellipse(q_est_vals(1,i), q_est_vals(2,i), P_vals{i}(1:2,1:2), 0.95, [0.9290 0.6940 0.1250]);
+        elseif i==1
+            plot_ellipse(q_est_vals(1,i), q_est_vals(2,i), P_vals{i}(1:2,1:2), 0.95, [0.9290 0.6940 0.1250]);
+        end
+    end
+    plot(q_est_vals(1,1:iter_break), q_est_vals(2,1:iter_break), 'r--', 'LineWidth', 1.5, 'DisplayName','Robot Est Trajectory')
+    plot(q_real_vals(1,1:iter_break), q_real_vals(2,1:iter_break), 'b-', 'LineWidth', 1.5, 'DisplayName','Robot Real Trajectory')
+    hold off
+    xlabel('X Coordinate [m]')
+    ylabel('Y Coordinate [m]')
+    title('Error Ellipses of the Robot Estimated Position')
+    grid on
+    
+% Plot covariance matrix - diagonal elements
+figure 
+    hold on
+    for i = 1:iter_break
+        sigma_xx(i) = P_vals{i}(1,1);
+        sigma_yy(i) = P_vals{i}(2,2);
+        sigma_tt(i) = P_vals{i}(3,3);
+    end
+
+    plot(time(1:iter_break), sigma_xx, 'Color', 'r', 'DisplayName', '$\sigma_{xx}^2$');
+    plot(time(1:iter_break), sigma_yy, 'Color', 'b', 'DisplayName', '$\sigma_{yy}^2$');
+    plot(time(1:iter_break), sigma_tt, 'Color', 'g', 'DisplayName', '$\sigma_{\theta\theta}^2$');
+    
+    xlabel('Time [s]')
+    ylabel('Covariance Matrix Diagonal Elements')
+    legend('Interpreter', 'latex', 'Location', 'best')
+    grid on
+    hold off
+    
+
+% Plot in a 3D-view the field distribution
+figure
+    surf(X,Y,Z)
+
+%% Usefull functions for plotting purposes
+function plot_ellipse(x, y, P, conf, color)
+    %PLOT_ELLIPSE Plot a covariance ellipse
+    % Inputs:
+    %   x, y: Center of the ellipse
+    %   P: Covariance matrix
+    %   conf: Confidence interval (0.95 for 95% confidence)
+    %   color: Color of the ellipse
+    
+    [eigVec, eigVal] = eig(P);
+
+    if eigVal(1,1)>eigVal(2,2)  % get the highest eigenvalue index
+        a = sqrt(eigVal(1,1));  % half-major axis length
+        b = sqrt(eigVal(2,2));  % half-minor axis length
+        theta = atan2(eigVec(2,1), eigVec(1,1)); % ellipse angle (radians)
+    else
+        a = sqrt(eigVal(2,2));  % half-major axis length
+        b = sqrt(eigVal(1,1));  % half-minor axis length
+        theta = atan2(eigVec(2,2), eigVec(1,2)); % ellipse angle (radians)
+    end
+
+    k = sqrt(-2 * log(1 - conf)); % 95% confidence
+
+    a95 = k * a; % 95% confidence
+    b95 = k * b; % 95% confidence
+
+    % Plot the ellipse
+    t = linspace(0, 2*pi, 100);
+    ellipse = [cos(t) * a; sin(t) * b];
+    ellipse95 = [cos(t) * a95; sin(t) * b95];
+    R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+    ellipse = R * ellipse;
+    ellipse95 = R * ellipse95;
+
+    fill(x + ellipse(1,:), y + ellipse(2,:), color, 'FaceAlpha', 0.2);
+    plot(x + ellipse95(1,:), y + ellipse95(2,:),'--', 'Color', color, 'LineWidth', 1.5);
+
+    % Create dummy plots for legend entries
+    xdummy=NaN;
+    ydummy=NaN;
+    h1 = fill(xdummy, ydummy, color, 'FaceAlpha', 0.2, 'DisplayName', 'Covariance ellipse'); % Example for contour
+    h2 = plot(xdummy, ydummy, '--', 'Color', color, 'DisplayName', '95\% Confidence ellipse '); % Example for error ellipse
+    % Add more dummy plots as needed for other legend entries
+
+    % Create custom legend
+    legend([h1, h2], 'Interpreter', 'latex','Location', 'best');
+
+end
 
 
