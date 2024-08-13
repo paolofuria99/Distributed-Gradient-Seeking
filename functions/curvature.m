a4e6ef31f3e27edfc497d2961689432128844f8b
Paolo Furia <81318686+paolofuria99@users.noreply.github.com>
2024-08-14 01:02:34 +0200

    Update curvature.m
    Bug fixed

--- functions/curvature.m
+++ functions/curvature.m
@@ -1,4 +1,4 @@
-function k=curvature(Z,p)
+function [k]=curvature(Z,p,x_range,y_range)
     %CURVATURE Compute the curvature of a function at a point
     % INPUTS:
     % f - Function handle
@@ -13,17 +13,17 @@ function k=curvature(Z,p)
     [fxx,fxy]= gradient(fx);
     [fyx,fyy]= gradient(fy);    
     
-    k = (fxx .* fy.^2 -fx.*fy.*(fxy + fyx) + fyy .* fx.^2) ./ ((fx.^2 + fy.^2).^(3/2));
+    k = (fxx.*fy.^2 - fx.*fy.*(fxy + fyx) + fyy .* fx.^2) ./ ((fx.^2 + fy.^2).^(3/2));
     
-    if nargin==2
+    if nargin>=2
         % Find the value of the gradient of a multivariate function at a
         % specified point
         x0 = p(1);
         y0 = p(2);
 
         % Find the index of the specified point in the grid
-        [~, ix] = min(abs(x - x0));  % Closest x-index
-        [~, iy] = min(abs(y - y0));  % Closest y-index
+        [~, ix] = min(abs(x_range - x0));  % Closest x-index
+        [~, iy] = min(abs(y_range - y0));  % Closest y-index
         
         % Get the curvature at the specified point
         curvature_at_point = k(iy, ix);  % Get the curvature value
