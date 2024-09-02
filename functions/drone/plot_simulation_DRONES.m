e5f1f2d2230bb7cf51527dec67e0bce7a3095c9c
lorecol <lorenzo.colturaato@gmail.com>
2024-09-02 16:05:35 +0200

    fixed animation of drones movement

--- functions/drone/plot_simulation_DRONES.m
+++ functions/drone/plot_simulation_DRONES.m
@@ -3,24 +3,43 @@ if ANIMATE_DRONES
     theta = linspace(0, 2*pi, 100);
     circle_x = params.radius*cos(theta);
     circle_y = params.radius*sin(theta);
-    figure;
-    hold on;
+    kfig = kfig + 1;
+    figure(kfig);
+    %set(gcf, 'WindowState', 'maximized');
+    set(gcf, 'Position', [100 100 1200 720]);
     plot(circle_x, circle_y, 'k--', 'DisplayName','Drones initialization region');
-    axis([-params.ENV_SIZE, params.ENV_SIZE, -params.ENV_SIZE,  params.ENV_SIZE]);
+    hold on;
+    axis([-params.ENV_SIZE, params.ENV_SIZE, -params.ENV_SIZE, params.ENV_SIZE]);
     xlabel('X');
     ylabel('Y');
     title('Unicycle Robot Gradient Descent to Find Gaussian Peak');
-    grid on;
-    contour(X, Y, Z, 30);
+    contour(X, Y, Z, 30);  % Create the contour plot
     colorbar;
+    grid on;
+    
+    % Preallocate plot handles for the trajectories
+    h_real_traj = plot(NaN, NaN, 'b-', 'LineWidth', 2, 'DisplayName', 'Robot Real Traj.');
+    h_est_traj = plot(NaN, NaN, 'r--', 'LineWidth', 2, 'DisplayName', 'Robot Est Traj.');
+    
     for i = 1:length(time(1:iter_break))
-        agent=robot.PlotAgent(q_ROBOT_real_vals(:,i));
-        plot(q_ROBOT_real_vals(1, 1:i), q_ROBOT_real_vals(2, 1:i), 'b-', 'LineWidth', 2);
+        % Update the trajectories
+        set(h_real_traj, 'XData', q_ROBOT_real_vals(1, 1:i), 'YData', q_ROBOT_real_vals(2, 1:i));
+        set(h_est_traj, 'XData', q_ROBOT_est_vals(1, 1:i), 'YData', q_ROBOT_est_vals(2, 1:i));
+        
+        % Plot the agent and keep the handle
+        agent = robot.PlotAgent(q_ROBOT_real_vals(:,i));
+        % Plot the drones and jeep the handle
         for idx = 1:params.N_agents
             DRONE(idx) = drone(idx).PlotDrone(q_real_drones_vals(:,i,idx));
             plot(q_real_drones_vals(1,1:i,idx),q_real_drones_vals(2,1:i,idx), 'r-','LineWidth',1.5);
         end
-        pause(dt);
+        
+        % Redraw the plot only every few iterations or after significant changes
+        if mod(i, 3) == 0 || i == length(time(1:iter_break))
+            drawnow;
+        end
+        
+        % Only delete the agent handle if necessary
         if ishandle(agent)
             delete(agent);
         end
@@ -46,7 +65,8 @@ end
 G = graph(sourceNodes, targetNodes);
 
 % Plot the graph
-figure;
+kfig = kfig + 1;
+figure(kfig);
 plot(G, 'Layout', 'force');
 title('Graph Representation of Connections');
 
@@ -56,7 +76,8 @@ title('Graph Representation of Connections');
 theta = linspace(0, 2*pi, 100);
 circle_x = params.radius*cos(theta);
 circle_y = params.radius*sin(theta);
-figure
+kfig = kfig + 1;
+figure(kfig);
 hold on
 h(1) = plot(circle_x, circle_y, 'k--', 'DisplayName','Drones initialization region');
 contour(X, Y, Z, 30, 'DisplayName','Scalar Field')
@@ -74,7 +95,8 @@ legend(h(1:4),'Location','best')
 grid on
 
 % Plot norm on estimation error before and after consensus
-figure;
+kfig = kfig + 1;
+figure(kfig);
 for idx = 1:params.N_agents
     h(idx) = plot(1:iter_break,error(:,:,idx),"LineWidth",0.1,"DisplayName",sprintf("Drone %d est.",idx));
     hold on;
